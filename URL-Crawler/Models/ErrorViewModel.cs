48bffd72362772295d4feac14873c01914c97cba
Brandon Hurler <brandonhurler@outlook.com>
2022-09-03 13:51:14 -0400

    Initial .NET 6 MVC project setup init.

---
+++ URL-Crawler/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace URL_Crawler.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
