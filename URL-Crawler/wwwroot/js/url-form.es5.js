45c41d31bc45e30eeef978f3975102a572a5d8ce
Brandon Hurler <brandonhurler@outlook.com>
2022-09-04 12:41:10 -0400

    Minifying url-form JS for optimization.

---
+++ URL-Crawler/wwwroot/js/url-form.es5.js
@@ -0,0 +1,49 @@
+﻿'use strict';
+
+$(document).ready(function () {
+    var form = $('.js-ajax-form');
+    var contentArea = $("#content-area");
+
+    // Use event delegation to correctly target submit if partial has been reloaded.
+    form.on('click', ':submit', function (e) {
+        // Prevent standard post-back for SPA AJAX submit
+        e.preventDefault();
+
+        post(form, contentArea, initializeCarousel);
+    });
+});
+
+var post = function post(form, contentArea, initialize) {
+    var validationArea = form.find("#validation-area");
+
+    $.ajax({
+        type: 'POST',
+        url: form.attr('action'),
+        data: form.serialize(),
+        dataType: 'json',
+        success: function success(data) {
+            if (!data.success) {
+                // Update form with data validation errors
+                form.html(data.payload);
+            } else {
+                // Clear errors on success if any
+                validationArea.html("");
+
+                // Display content
+                contentArea.html(data.payload);
+
+                // Can init any passed function into form post to bind events to newly generated partial content
+                initialize();
+            }
+        },
+        error: function error(data) {
+            // Show stacktrace, can be replaced by user-friendly errors
+            contentArea.html(data);
+        }
+    });
+};
+
+var initializeCarousel = function initializeCarousel() {
+    $("#image-carousel").carousel();
+};
+
