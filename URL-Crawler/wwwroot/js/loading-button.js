a5e9aa375e30985daa0dce7f8c90e3dac1229501
Brandon Hurler <brandonhurler@outlook.com>
2022-09-06 19:40:29 -0400

    Adding loading spinner to button to indicate call is running.

---
+++ URL-Crawler/wwwroot/js/loading-button.js
@@ -0,0 +1,9 @@
+﻿$(document).ajaxStart(function () {
+    $(".js-btn-submit:submit").hide();
+    $(".js-btn-submit:disabled").show();
+});
+
+$(document).ajaxStop(function () {
+    $(".js-btn-submit:submit").show();
+    $(".js-btn-submit:disabled").hide();
+});
